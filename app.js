fcc0a78d36553ca7a20747750168412f9b415a9f
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-02-05 13:37:31 -0800

    created objects for each location

---
+++ app.js
@@ -0,0 +1,19 @@
+const cookiesAirport = {
+    location: 'PDX Airport'
+};
+
+const cookiesPioneer = {
+    location: 'Pioneer Square'
+};
+
+const cookiesPowells = {
+    location: 'Powell\'s'
+};
+
+const cookiesJohn = {
+    location: 'St. John\'s'
+};
+
+const cookiesWaterfront = {
+    location: 'Waterfront'
+};
