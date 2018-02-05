943198b2ca70ba404c20824170af59a77bd8d3b4
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-02-05 14:50:58 -0800

    added customerHour to all objects

--- app.js
+++ app.js
@@ -17,25 +17,53 @@ const cookiesPioneer = {
     minHourly: 3,
     maxHourly: 24,
     averageCookies: 1.2,
+    customersHour: function () {
+        const min = Math.ceil(this.minHourly);
+        const max = Math.floor(this.maxHourly);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 };
+
+console.log(cookiesPioneer.customersHour());
 
 const cookiesPowells = {
     location: 'Powell\'s',
     minHourly: 11,
     maxHourly: 38,
     averageCookies: 3.7,
+    customersHour: function () {
+        const min = Math.ceil(this.minHourly);
+        const max = Math.floor(this.maxHourly);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 };
+
+console.log(cookiesPowells.customersHour());
 
 const cookiesJohn = {
     location: 'St. John\'s',
     minHourly: 20,
     maxHourly: 38,
     averageCookies: 2.3,
+    customersHour: function () {
+        const min = Math.ceil(this.minHourly);
+        const max = Math.floor(this.maxHourly);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 };
+
+console.log(cookiesJohn.customersHour());
 
 const cookiesWaterfront = {
     location: 'Waterfront',
     minHourly: 2,
     maxHourly: 16,
     averageCookies: 4.6,
+    customersHour: function () {
+        const min = Math.ceil(this.minHourly);
+        const max = Math.floor(this.maxHourly);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 };
+
+console.log(cookiesWaterfront.customersHour());
