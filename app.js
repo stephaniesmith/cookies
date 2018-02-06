c8d4c9bf32f9b181c735543b922f3a3c1d84f389
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-02-06 14:02:46 -0800

    added objects powells and johns

--- app.js
+++ app.js
@@ -52,120 +52,20 @@ pioneer.calcCookies();
 pioneer.totalCookies();
 pioneer.renderCookies();
 
+const powells = new Store ('Powell\'s', 11, 38, 3.7, 'powells');
+console.log(powells);
 
-// const cookiesPioneer = {
-//     location: 'Pioneer Square',
-//     minHourly: 3,
-//     maxHourly: 24,
-//     averageCookies: 1.2,
-//     hourlyArray: [],
-//     calcCookies: function () {
-//         const min = Math.ceil(this.minHourly);
-//         const max = Math.floor(this.maxHourly);
-//         for (let i = 0; i < 15; i++) {
-//             const customersPerHour =  Math.floor(Math.random() * (max - min + 1)) + min;
-//             const cookiesPerHour = Math.round(customersPerHour * this.averageCookies);
-//             this.hourlyArray.push(cookiesPerHour);
-//         }
-//     },
-//     totalCookies: function () {
-//         let total = 0;
-//         for (let j = 0; j < 15; j++) {
-//             total = total + this.hourlyArray[j];
-//         }
-//         this.hourlyArray.push(total);
-//     },
-//     renderCookies: function () {
-//         for (let i = 0; i < this.hourlyArray.length; i++) {
-//             const list = document.getElementById('pioneer');
-//             const li = document.createElement('li');
-//             li.textContent = time[i] + this.hourlyArray[i] + ' cookies';
-//             list.appendChild(li);
-//         }
-//     }
-// };
+powells.calcCookies();
+powells.totalCookies();
+powells.renderCookies();
 
-// console.log('this is the array: ' + cookiesPioneer.hourlyArray);
-// console.log('this is the function: ' + cookiesPioneer.calcCookies());
-// console.log('this is the array: ' + cookiesPioneer.hourlyArray);
-// console.log('this is the total: ' + cookiesPioneer.totalCookies());
-// console.log('render' + cookiesPioneer.renderCookies());
+const johns = new Store ('St. John\'s', 20, 38, 2.3, 'johns');
+console.log(johns);
 
-// const cookiesPowells = {
-//     location: 'Powell\'s',
-//     minHourly: 11,
-//     maxHourly: 38,
-//     averageCookies: 3.7,
-//     hourlyArray: [],
-//     calcCookies: function () {
-//         const min = Math.ceil(this.minHourly);
-//         const max = Math.floor(this.maxHourly);
-//         for (let i = 0; i < 15; i++) {
-//             const customersPerHour =  Math.floor(Math.random() * (max - min + 1)) + min;
-//             const cookiesPerHour = Math.round(customersPerHour * this.averageCookies);
-//             this.hourlyArray.push(cookiesPerHour);
-//         }
-//     },
-//     totalCookies: function () {
-//         let total = 0;
-//         for (let j = 0; j < 15; j++) {
-//             total = total + this.hourlyArray[j];
-//         }
-//         this.hourlyArray.push(total);
-//     },
-//     renderCookies: function () {
-//         for (let i = 0; i < this.hourlyArray.length; i++) {
-//             const list = document.getElementById('powells');
-//             const li = document.createElement('li');
-//             li.textContent = time[i] + this.hourlyArray[i] + ' cookies';
-//             list.appendChild(li);
-//         }
-//     }
-// };
+johns.calcCookies();
+johns.totalCookies();
+johns.renderCookies();
 
-// console.log('this is the array: ' + cookiesPowells.hourlyArray);
-// console.log('this is the function: ' + cookiesPowells.calcCookies());
-// console.log('this is the array: ' + cookiesPowells.hourlyArray);
-// console.log('this is the total: ' + cookiesPowells.totalCookies());
-// console.log('render' + cookiesPowells.renderCookies());
-
-// const cookiesJohn = {
-//     location: 'St. John\'s',
-//     minHourly: 20,
-//     maxHourly: 38,
-//     averageCookies: 2.3,
-//     hourlyArray: [],
-//     calcCookies: function () {
-//         const min = Math.ceil(this.minHourly);
-//         const max = Math.floor(this.maxHourly);
-//         for (let i = 0; i < 15; i++) {
-//             const customersPerHour =  Math.floor(Math.random() * (max - min + 1)) + min;
-//             const cookiesPerHour = Math.round(customersPerHour * this.averageCookies);
-//             this.hourlyArray.push(cookiesPerHour);
-//         }
-//     },
-//     totalCookies: function () {
-//         let total = 0;
-//         for (let j = 0; j < 15; j++) {
-//             total = total + this.hourlyArray[j];
-//         }
-//         this.hourlyArray.push(total);
-//     },
-//     renderCookies: function () {
-//         for (let i = 0; i < this.hourlyArray.length; i++) {
-//             const list = document.getElementById('johns');
-//             const li = document.createElement('li');
-//             li.textContent = time[i] + this.hourlyArray[i] + ' cookies';
-//             list.appendChild(li);
-//         }
-//     }
-// };
-
-// console.log('this is the array: ' + cookiesJohn.hourlyArray);
-// console.log('this is the function: ' + cookiesJohn.calcCookies());
-// console.log('this is the array: ' + cookiesJohn.hourlyArray);
-// console.log('this is the total: ' + cookiesJohn.totalCookies());
-// console.log('render' + cookiesJohn.renderCookies());
 
 // const cookiesWaterfront = {
 //     location: 'Waterfront',
