8d70884b8f3bb148d67aee774e6ae41ade10f683
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-02-08 17:12:01 -0800

    added merch and updated fonts

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const time = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total: '];
+const time = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
 
-const footer = ['total', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const footer = ['Total', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 function Store(location, minHourly, maxHourly, averageCookies) {
     this.location = location;
